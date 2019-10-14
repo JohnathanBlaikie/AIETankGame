f8ec745d9c9a52d7e20048283550c6b8968933ad
Johnathan Blaikie <johnathanblaikie@gmail.com>
2019-10-14 09:38:22 -0700

    first real push

---
+++ ConsoleApp1/SpriteObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MathHelpers;
+using System.Collections.Generic;
+using System.Text;
+using Raylib;
+using rl = Raylib.Raylib;
+using static Raylib.Raylib;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class SpriteObject : SceneObject
+    {
+        Texture2D texture = new Texture2D();
+        Image image = new Image();
+
+        public float Width
+        {
+            get { return texture.width; }
+
+        }
+
+        public float Height
+        {
+            get { return texture.height; }
+        }
+
+        public SpriteObject()
+        {
+
+        }
+
+        public void Load(string filename)
+        {
+            Image img = LoadImage(filename);
+            texture = LoadTextureFromImage(img);
+
+        }
+        public override void OnDraw()
+        {
+            float rotation = (float)Math.Atan2(globalTransform.m2, globalTransform.m1);
+            
+            DrawTextureEx(texture, new Vector2(globalTransform.m7, globalTransform.m8),
+                rotation * (float)(180.0f / Math.PI), 1, Color.WHITE);
+        }
+    }
+}
